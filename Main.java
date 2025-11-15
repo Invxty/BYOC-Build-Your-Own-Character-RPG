adf1b815832e79a47f605489c45dfdb309895d90
invxty <nathanielabrigo1220@gmail.com>
2025-11-15 23:49:18 +0800

    added Boss abilities

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 
 class Player {
-    static int hp = 100; 
+    static int hp = 1000; 
 }
 
 
@@ -31,32 +31,122 @@ class MonsterClass {
 
 class Boss extends MonsterClass { 
     int phases;
-    String ability; 
-  
-    Boss(String name, int atk, int hp, int def, int sp, int phases, String ability) {
+
+    Boss(String name, int atk, int hp, int def, int sp, int phases) {
         
         super(name, atk, hp, def, sp); 
         this.phases = phases;
-        this.ability = ability;
     }
-
     
-    public void abilityUse() {
-        System.out.println(this.name + " uses a special ability!");
-        System.out.println(this.name + this.ability);
+    public void takeTurn(){
+        super.attack();
     }
 }
 
 
+ class BoarKing extends Boss{
+    private boolean isCharging = false; 
+    private int chargeCount = 0;
+
+    public BoarKing(String name, int atk, int hp, int def, int sp, int phases) {
+        super(name, atk, hp, def, sp, phases);
+    }
+
+    @Override
+    public void takeTurn() {
+        if (isCharging) {
+            handleChargingLogic();
+        } else {
+
+            if (Math.random() < 0.3) {
+                startCharging();
+            } else {
+                super.attack(); 
+            }
+        }
+    }
+
+    private void startCharging() {
+        System.out.println(this.name + " scrapes the ground... (Charging Ability)");
+        isCharging = true;
+        chargeCount = 0;
+    }
+
+    private void handleChargingLogic() {
+        chargeCount++;
+        if (chargeCount >= 2) {
+            System.out.println(">>> " + this.name + " UNLEASHES BOAR SMASH! <<<");
+            Player.hp -= 250; // Huge damage
+            System.out.println("Player took 250 damage!");
+            isCharging = false; // Reset state
+        } else {
+            System.out.println(this.name + " is building up power... (Round " + chargeCount + ")");
+        }
+    }
+}
+
+class Litch extends Boss{
+    private int Rounds = 0;
+    private boolean Curse = false;
+
+    public Litch(String name, int atk, int hp, int def, int sp, int phases) {
+        super(name, atk, hp, def, sp, phases);
+    }
+    @Override
+    public void takeTurn() {
+        if (Curse) {
+            if(Rounds > 0){
+            handleCurse();
+            super.attack();
+            }
+            else{
+                System.out.println("Curse has taken effect... Game over...");
+                Player.hp -= Player.hp;
+            }
+        } 
+        else {
+            startCurse();
+            }
+        }
+    
+    private void startCurse() {
+        System.out.println(this.name + " cursed player! (15 rounds till death)");
+        Curse = true;
+        Rounds = 15;
+    }
+
+    private void handleCurse(){
+        Rounds--;
+        System.out.println("You are cursed... (" + Rounds + " Rounds remaining!)");
+    }
+    }
+    
+
+class SkelKing extends Boss{
+    private int Armor = 500;
+    private int ArmorDurability = 3;
+    public SkelKing(String name, int atk, int hp, int def, int sp, int phases) {
+        super(name, atk, hp, def, sp, phases);
+    }
+
+
+    private void ArmorCondition(){
+        if(ArmorDurability > 0){
+            if(Armor == 0){
+                
+            }
+        }
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        
-        Boss Boarking = new Boss("Boar king", 50, 1000, 20, 10, 0," is now charing a heavy attack for 2 rounds!");
+  
+        Boss currentBoss = new BoarKing("Litch", 60, 1000, 20, 10, 0);
         MonsterClass slime = new MonsterClass("Slime", 25, 500, 10, 5);
-        
-        Boarking.attack(); //sample boss attack
-        Boarking.abilityUse(); //sample ability use, not sure pa pano 'to iimplement
-
-        slime.attack(); //Common enemy attack
+        while (Player.hp > 0 && currentBoss.hp > 0) { 
+            currentBoss.takeTurn();
     }
+}
 }
